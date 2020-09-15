42fccb0ef8dcb14b3275db6003fb68ff90d3620b
María Victoria Alina Bartak <victoriaalina89@gmail.com>
2020-09-15 19:24:51 +1200

    started about us page, changed images and general design home page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require ('express');
 const app = express();
 const serveStatic = require ('serve-static');
-
-
-
+const basicAuth = require('express-basic-auth');
+const { response } = require('express');
 
 //setings
 app.set('port', 8000);
@@ -12,23 +11,22 @@ app.set('view engine', 'ejs');
 
 app.engine('html', require('ejs').renderFile);
 
-//static files
-app.use(serveStatic(__dirname + '/public'));
+app.use(serveStatic(__dirname + '/../public'));
 
-// app.use(session({
-//     resave: true, 
-//     saveUninitialized: true,
-//     secret: 'secret'
-// }));
-
-//routes
+app.use(basicAuth({
+    users: { 'selwyn': 'selwyn2016' },
+    challenge: true,
+}));
 
 app.get("/", (require, response) => {
-    response.render("index.ejs")
-})
+    response.render("index.ejs");
+});
 
+app.get("/about-us", (require, response) => {
+    response.render("aboutUs.ejs");
+});
 
 //listening the server
 app.listen(app.get('port'), () => {
     console.log('server on port', app.get('port'));
-})
+});
